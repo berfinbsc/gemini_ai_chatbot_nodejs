b1c0224a82e6880e8c1f377e33be6e9ae157352e
berfinbsc <berfinbasci4701@gmail.com>
2024-05-23 01:51:51 +0200

    23-commit

--- Server.js
+++ Server.js
@@ -2,19 +2,15 @@ const express = require('express');
 const cors = require('cors'); // cors paketini import edin
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const app = express();
-// CORS ayarlarını yapılandırın
 app.use(cors({
   origin: '*'
 }));
-// Gövde verilerini ayrıştırmak için middleware
 app.use(express.json());
 require('dotenv').config();
-
 const api =process.env.API_KEY;
-
-
-
 const genAI = new GoogleGenerativeAI(api);
+
+
 
 
 
@@ -22,21 +18,37 @@ app.get('/',  (req, res) => {
     res.status(200).json({ message: "hello world" });
 });
 
+
+
+
 app.post('/message', async (req, res) => {
   const { message, history } = req.body;
+console.log(history);
+console.log(message);
 
   const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
   const chat = model.startChat({
     history:history,
     generationConfig: {
-      maxOutputTokens: 100,
+      maxOutputTokens: 500,
     },
   });
 
+  
+
+try {
+  
   const result = await chat.sendMessage(message);
   const response = await result.response;
   const text = response.text();
-  res.json({history});
+  console.log("text :: :: ",text);
+  console.log("response :: ::",response);
+  res.send(text);
+
+} catch (error) {
+  console.log(error);
+}
+
 
 });
 
